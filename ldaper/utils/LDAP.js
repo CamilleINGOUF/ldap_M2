3dba13ec7dbc36a7d7aacf014b9a66e6515ddeab
CamilleINGOUF <ingouf.camille@gmail.com>
2019-03-12 14:05:34 +0100

    pfieruofgerui

--- ldaper/utils/LDAP.js
+++ ldaper/utils/LDAP.js
@@ -1,92 +1,96 @@
-
 var ldaper = require('ldapjs')
 const assert = require('assert')
 
 let client = null
 var loginConnected = ''
 // +',dc=bla,dc=com'
-async function connect (login, pass) {
+async function connect(login, pass) {
   loginConnected = login
   return await connectClient(login, pass).then(res => res)
 }
 
 async function connectClient(login, pass, callback) {
-  if(login !== 'Admin')
-    login+=',ou=people'
+  if (login !== 'Admin')
+    login += ',ou=people'
   return new Promise((resolve, reject) => {
     client = ldaper.createClient({
       url: 'ldap://ldap:389'
     })
-    client.bind('cn='+login+',dc=bla,dc=com', pass, (err) => {
-        if(err) {
-          client.unbind();
-          reject(false)
-        } else {
-          resolve(true)
-        }
+    client.bind('cn=' + login + ',dc=bla,dc=com', pass, (err) => {
+      if (err) {
+        client.unbind();
+        reject(false)
+      } else {
+        resolve(true)
+      }
     })
   })
 }
 
-async function disconnect () {
+async function disconnect() {
   return new Promise((resolve, reject) => {
     client.unbind()
-    resolve({result: true})
+    resolve({
+      result: true
+    })
   })
 }
 
-async function search (ou) {
+async function search(ou) {
   return new Promise((resvole, reject) => {
     var opts = {
-      filter: loginConnected === 'Admin' ? '(objectclass=*)' : '(&(uid='+loginConnected+'))',
+      filter: loginConnected === 'Admin' ? '(objectclass=*)' : '(&(uid=' + loginConnected + '))',
       scope: 'sub'
     };
-    client.search('ou='+ou+',dc=bla,dc=com', opts, function(err,res) {
+    client.search('ou=' + ou + ',dc=bla,dc=com', opts, function (err, res) {
       assert.ifError(err)
       let entries = []
-      res.on('searchEntry', function(entry) {
+      res.on('searchEntry', function (entry) {
         entries.push(entry.object)
       });
 
-      res.on('searchReference', function(referral) {
+      res.on('searchReference', function (referral) {
         console.log('referral: ' + referral.uris.join());
       });
 
-      res.on('error', function(err) {
+      res.on('error', function (err) {
         console.error('error: ' + err.message);
         reject(err)
       });
-      
-      res.on('end', function(result) {
-        entries.push({login: loginConnected, 'admin': (loginConnected === 'Admin')})
+
+      res.on('end', function (result) {
+        entries.push({
+          login: loginConnected,
+          'admin': (loginConnected === 'Admin')
+        })
         resvole(entries)
       });
     })
   })
 }
 
-async function add (dn, user) {
+async function add(dn, user) {
   return new Promise((resolve, reject) => {
-    client.add(dn, user, function(err,res) {
-      if(err) reject(err)
+    client.add(dn, user, function (err, res) {
+      if (err) reject(err)
       else resolve(res)
     })
   })
 }
 
-async function del (dn) {
+async function del(dn) {
   return new Promise((resolve, reject) => {
-    client.del(dn, function(err,res) {
-      if(err) reject(err)
+    client.del(dn, function (err, res) {
+      if (err) reject(err)
       else resolve(res)
     })
   })
 }
 
-async function put (dn, change) {
+async function put(dn, change) {
   return new Promise((resolve, reject) => {
-    client.modify(dn, change, function(err,res) {
-      if(err) reject(err)
+    client.modify(dn, change, function (err, res) {
+      if (err) reject(err)
       else resolve(res)
     })
   })
